a6f982ef72becd2ea57902114e79893d79a1d7d3
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-09-18 17:14:19 +0530

    Add new sakamoto

---
+++ src/cog.py
@@ -0,0 +1,71 @@
+# cog.py
+import discord
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+import random
+
+sassy = [
+    "Don't make me do your petty work, kid."
+]
+
+def get_rand_sassy():
+    return sassy[random.randint(0, len(sassy)-1)]
+
+class Slash(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(
+        name="test",
+        description='just a test',
+        guild_ids=[568123114349920256],
+        options=[
+            create_option(
+                name="integ",
+                description="pick an integer, any integer!",
+                required=True,
+                option_type=4
+            )
+        ]
+        )
+    async def _test(self, ctx: SlashContext, integ:int):
+        embed = discord.Embed(title=("Is your integer " + str(integ) + "?"))
+        await ctx.send(content="test", embeds=[embed])
+
+class Gamba(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(
+        name="flip-coin",
+        description='get a coin flip',
+        guild_ids=[568123114349920256],
+        options=[]
+        )
+    async def _flipcoin(self, ctx: SlashContext, dummy=None):
+        if random.randint(0,1) == 0:
+            coin = "Heads"
+            thumb = "https://i.imgur.com/NusQZZR.png"
+        else:
+            coin = "Tails"
+            thumb = "https://i.imgur.com/IkJzBOX.png"
+
+        embed = discord.Embed(description=get_rand_sassy(), title=coin)
+        embed.set_thumbnail(url=thumb)
+        await ctx.send(embeds=[embed])
+
+    @cog_ext.cog_slash(
+        name="roll-dice",
+        description='get a dice roll',
+        guild_ids=[568123114349920256],
+        options=[]
+        )
+    async def _rolldice(self, ctx: SlashContext, dummy=None):
+        embed = discord.Embed(description=get_rand_sassy(), title=str(random.randint(1, 6)))
+        embed.set_thumbnail(url="https://i.imgur.com/IkJzBOX.png")
+        await ctx.send(embeds=[embed])
+
+def setup(bot):
+    bot.add_cog(Slash(bot))
+    bot.add_cog(Gamba(bot))
--- src/main.py
+++ src/main.py
@@ -1,13 +1,36 @@
-# bot.py
-# Copyright 2020 Sankalp Gambhir
-
-# Comment format
-# <Description>. %<Expected_Type>
+# Sakamoto
 
 import os
-from sakamoto_client import *
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+from dotenv import load_dotenv, find_dotenv
+import cog
 
+# basic objects
+
+class EnvData:
+    def __init__(self):
+        self._token = os.getenv("DISCORD_BOT_TOKEN")
+
+        # error reporting 
+        self._errid = int(os.getenv("ERROR_HANDLER"))    # who to report to? %Snowflake
+        self._errchan = int(os.getenv("ERROR_CHANNEL"))  # where? %Snowflake
+
+        return
 
 if __name__ == '__main__':
-    # login to discord only if this is the head process
-    client = sakamoto_client(env)
+    # load environment variables
+    load_dotenv(find_dotenv())
+    env = EnvData()
+
+    # setup bot
+    
+    client = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+    slash = SlashCommand(client, sync_commands=True, override_type=True)
+
+    cog.setup(client)
+
+    # login
+    client.run(env._token)
