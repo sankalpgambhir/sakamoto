be30e2cb2482503cf5707710778ff75f30e5e300
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-09-20 17:49:00 +0530

    Make reminder skeleton

--- src/cog.py
+++ src/cog.py
@@ -92,7 +92,27 @@ class Emote(commands.Cog):
         # send emote
         await ctx.send("UNDEF")
 
+
+class Reminder(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(
+        name="setreminder",
+        description='set a reminder',
+        guild_ids=[568123114349920256],
+        options=[
+            # add choices here
+        ]
+        )
+    async def _setreminder(self, ctx: SlashContext, emote=None):
+        # set a reminder
+        # queue it
+        # send details
+        await ctx.send("UNDEF")
+
 def setup(bot):
     bot.add_cog(Test(bot))
     bot.add_cog(Gamba(bot))
     bot.add_cog(Emote(bot))
+    bot.add_cog(Reminder(bot))
