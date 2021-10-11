80e51299b05f8153465568769251dd4a2babab85
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2021-10-11 14:47:23 +0530

    Autoemote now in testing phase

--- src/cog.py
+++ src/cog.py
@@ -5,6 +5,7 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
 import random
 import pandas
+import string
 
 sassy = [
     "Don't make me do your petty work, kid."
@@ -76,12 +77,25 @@ class Gamba(commands.Cog):
         embed.set_thumbnail(url="https://i.imgur.com/AYCmeEa.jpg")
         await ctx.send(embeds=[embed])
 
+# read emotes and construct list
+emotelist = pandas.read_csv('emoji.csv', sep='\t').values
+emotelist = [[x[0].lower(), x[1]] for x in emotelist]
+emotelist.sort(key=lambda x: x[0])
+# construct choices for autofill
+choice = {}
+for l in string.ascii_lowercase[:-1]:
+    choice[l] = create_option(
+                name=l,
+                description=l,
+                required=True,
+                option_type=3,
+                choices=[t[0] for t in emotelist if t[0][0] == l]
+            )
+    choice[l].update({'value' : 'TEST'})
 
 class Emote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.emotelist = pandas.read_csv('emoji.csv', sep='\t').values
-        self.emotelist = [[x[0].lower(), x[1]] for x in self.emotelist]
 
     @cog_ext.cog_slash(
         name="emote",
@@ -99,12 +113,58 @@ class Emote(commands.Cog):
     async def _emote(self, ctx: SlashContext, which=""):
         # send emote
 
-        emotelink = next((x[1] for x in self.emotelist if x[0] == which.lower()), None)
+        emotelink = next((x[1] for x in emotelist if x[0] == which.lower()), None)
 
         if emotelink is not None:
             await ctx.send(emotelink)
         else:
             pass
+
+    @cog_ext.cog_slash(
+        name="autoemote",
+        description='send custom emotes',
+        guild_ids=[568123114349920256],
+        options=[
+            # create a group for each letter
+            create_option(
+                name=l,
+                description=l,
+                required=False,
+                option_type=3,
+                choices=[t[0] for t in emotelist if t[0][0] == l]
+            )
+            for l in string.ascii_lowercase[:-1]
+        ]
+        )
+    async def _autoemote(self, ctx: SlashContext, dummy = '', *args, **kwargs):
+        # send emote
+
+        for l in kwargs.values():
+            emotelink = next((x[1] for x in emotelist if x[0] == l.lower()), None)
+            if emotelink is not None:
+                break
+
+        if emotelink is not None:
+            await ctx.send(emotelink)
+        else:
+            pass
+    
+    @cog_ext.cog_slash(
+        name="listemote",
+        description='list all custom emotes',
+        guild_ids=[568123114349920256],
+        options=[]
+        )
+    async def _listemote(self, ctx: SlashContext, dummy = None):
+        # send emote
+
+        res = discord.Embed(title='Emote list', description=get_rand_sassy())
+
+        s = [e[0] for e in emotelist]
+
+        res.add_field(name = 'Emotes', value = ' '.join(s))
+
+        await ctx.send(embeds=[res])
 
 
 class Reminder(commands.Cog):
